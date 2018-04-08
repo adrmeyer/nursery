c6d23cdc9abca2d697200aa1a26110ecb4c9276d
Adrian Meyer <adrianmeyer77450@gmail.com>
2018-04-08 12:51:16 +0200

    first eclise push

---
+++ CRUD/src/main/java/com/daoapi/ChildDao.java
@@ -0,0 +1,13 @@
+package com.daoapi;
+
+import java.util.List;
+
+import com.entities.Child;
+
+public interface ChildDao {
+	public boolean saveOrUpdate(Child child);
+	 
+    public List<Child> list();
+ 
+    public boolean delete(Child child);
+}
---
+++ CRUD/src/main/java/com/daoimpl/ChildImpl.java
@@ -0,0 +1,40 @@
+package com.daoimpl;
+
+import java.util.List;
+
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.daoapi.ChildDao;
+import com.entities.Child;
+
+@Repository("ChildDao")
+@Transactional
+public class ChildImpl implements ChildDao {
+
+	@Autowired
+    SessionFactory session;
+	
+	public boolean saveOrUpdate(Child child) {
+        // TODO Auto-generated method stub
+        session.getCurrentSession().saveOrUpdate(child);
+
+        return true;
+    }
+ 
+    public List<Child> list() {
+        return session.getCurrentSession().createQuery("from Child").list();
+    }
+ 
+    public boolean delete(Child child) {
+        try {
+            session.getCurrentSession().delete(child);
+        } catch (Exception ex) {
+            return false;
+        }
+ 
+        return true;
+    }
+}
